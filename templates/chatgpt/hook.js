94943be1f913eee1418ed6e9cbb6d9c424065aa1
dhb <xx158@qq.com>
2024-11-04 15:23:47 +0800

    feat: 处理埋点数据

--- templates/chatgpt/hook.js
+++ templates/chatgpt/hook.js
@@ -10,7 +10,12 @@ function modifyUrl(url) {
 window.fetch = new Proxy(window.fetch, {
     apply: function(target, thisArg, argumentsList) {
         argumentsList[0] = modifyUrl(argumentsList[0])
-
+        if (argumentsList[0].indexOf('/ab/v1/rgstr') !== -1) {
+            const body = argumentsList[1] || argumentsList[1].body
+            if (body) {
+                argumentsList[1].body = argumentsList[1].body.replaceAll(window.location.host, 'chatgpt.com')
+            }
+        }
         return target.apply(thisArg, argumentsList).then(response => {
             if (response.status >= 401) {
                 return target.apply(thisArg, argumentsList)
